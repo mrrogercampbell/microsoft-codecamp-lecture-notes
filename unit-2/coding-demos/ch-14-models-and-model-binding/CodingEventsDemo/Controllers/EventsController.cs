507f2f38cc8a52f499d61013f007cdc097d2a442
Roger Campbell II <mr@rogercampbell.io>
2021-07-22 15:30:02 -0400

    Added chapter 14 demo

---
+++ unit-2/coding-demos/ch-14-models-and-model-binding/CodingEventsDemo/Controllers/EventsController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CodingEventsDemo.Data;
+using CodingEventsDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace coding_events_practice.Controllers
+{
+    public class EventsController : Controller
+    {
+    
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            ViewBag.events = EventData.GetAll();
+
+            return View();
+        }
+
+        // GET: /<controller>/Add
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        // POST: /<controller>/
+        [HttpPost("/Events/Add")]
+        public IActionResult AddNewEvent(Event newEvent)
+        {
+            //Event newEvent = new Event(eventName, eventDescription);
+
+            EventData.Add(newEvent);
+
+            return Redirect("/Events");
+        }
+
+        public IActionResult Delete()
+        {
+            ViewBag.events = EventData.GetAll();
+
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Delete(int[] eventIds)
+        {
+            foreach (int eventId in eventIds)
+            {
+                EventData.Remove(eventId);
+            }
+
+            return Redirect("/Events");
+        }
+
+        
+        public IActionResult GetSingleEvent()
+        {
+            ViewBag.events = EventData.GetAll();
+
+            return View("~/Views/Events/GetSingleEventForm.cshtml");
+        }
+
+        [HttpPost]
+        public IActionResult GetSingleEvent(int eventId)
+        {
+            ViewBag.events = EventData.GetById(eventId);
+
+            return View("~/Views/Events/Index.cshtml");
+        }
+    }
+}
---
+++ unit-2/coding-demos/ch-14-models-and-model-binding/CodingEventsDemo/Data/EventData.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using CodingEventsDemo.Models;
+
+namespace CodingEventsDemo.Data
+{
+    public class EventData
+    {
+        //static private List<Event> events = new List<Event>();
+
+        static private Dictionary<int, Event> _events = new Dictionary<int, Event>();
+
+        // Add
+        public static void Add(Event newEvent)
+        {
+            _events.Add(newEvent.Id, newEvent);
+        }
+
+
+        // GetAll
+        public static IEnumerable<Event> GetAll()
+        {
+            return _events.Values;
+        }
+
+
+        //// Remove
+        public static void Remove(int id)
+        {
+            _events.Remove(id);
+        }
+
+
+        // GetById
+        public static Event GetById(int id)
+        {
+
+            return _events[id];
+        }
+
+    }
+}
